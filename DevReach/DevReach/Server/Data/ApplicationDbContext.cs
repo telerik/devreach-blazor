0934d75a98470e6d153337e2cc214551fdad6bdf
Chris Sainty <chris@chrissainty.com>
2020-10-19 15:01:11 +0100

    Auth/form/validation

---
+++ DevReach/DevReach/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevReach.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace DevReach.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(new IdentityRole { Name = "User", NormalizedName = "USER", Id = Guid.NewGuid().ToString(), ConcurrencyStamp = Guid.NewGuid().ToString() });
+        }
+    }
+}
---
+++ DevReach/DevReach/Shared/Gig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevReach.Shared
+{
+    public class Gig
+    {
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public DateTime? Date { get; set; }
+        [Range(5, 1000)]
+        public decimal Payment { get; set; }
+        public bool HasBeenPaid { get; set; }
+    }
+}
